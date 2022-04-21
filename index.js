4ffcf37f23a71421a47eeb7144c405b72b52c087
abdullahshinwari <abdullah.shinwari@horizon.net.pk>
2022-04-21 15:12:08 +0500

    changes in index.js file for routes

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const { mongoos } = require('./db.js')
-var employeeController = require('./controllers/employeeController');
+// var employeeController = require('./app/controllers/employeeController');
 
 var app = express();
 app.use(bodyparser.json());
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
-app.use('/employees', employeeController);
+// app.use('/employees', employeeController);
+let employee_routes = require('./app/routes/employee');
+let lead_routes = require('./app/routes/leads');
+app.use(employee_routes);
+app.use(lead_routes);
+
+module.exports = app;
